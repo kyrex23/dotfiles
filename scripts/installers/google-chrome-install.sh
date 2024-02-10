ac1a13ee0dc3104bbcbc648fa5429d5449bed3a1
Kyrex <59597626+kyrex23@users.noreply.github.com>
2024-02-10 22:29:31 +0100

    feat: [scripts] improve google-chrome installer

--- scripts/installers/google-chrome-install.sh
+++ scripts/installers/google-chrome-install.sh
@@ -19,13 +19,31 @@ FILE_SRC="/etc/apt/sources.list.d/google-chrome.list"
 
 SYSTEM_ARCH="$(dpkg --print-architecture)"
 
+PACKAGE="google-chrome-stable"
+
 # ----------------------------------------------------------------------------------------------------------------------
 
 # Download the gpg key and store in a safe place
+if [ -e "$FILE_KEY" ]; then
+	echo "A gpg key was found at: $FILE_KEY"
+	echo "Renaming to: $FILE_KEY.old..."
+	sudo mv "$FILE_KEY" "$FILE_KEY".old
+	echo "Done!"
+	echo
+fi
+
+echo "Downloading a new gpg key and storing it at: $FILE_KEY..."
 wget -qO - $URL_KEY | gpg --dearmor | sudo tee $FILE_KEY > /dev/null
+echo "Done!"
+echo
 
 # Add the official repository signed by the downloaded key (safe way)
+echo "Adding the repository at: $FILE_SRC..."
 echo "deb [arch=$SYSTEM_ARCH signed-by=$FILE_KEY] $URL_SRC stable main" | sudo tee $FILE_SRC > /dev/null
+echo "Done!"
+echo
 
 # Update packages and install/update google-chrome stable
-sudo apt update && sudo apt install google-chrome-stable -y
+echo "Installing the latest stable version..."
+sudo apt update && sudo apt install "$PACKAGE" -y
+echo "Done!"
